3d21335b446d27f467ae46f42af9669c8ae8eb15
Daniel Wozniak <dlw_future@hotmail.com>
2018-10-23 13:16:16 -0300

    feat: Changes in screen layout

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View, StatusBar } from 'react-native'
+import { createStackNavigator, createTabNavigator } from 'react-navigation'
+import { Constants } from 'expo'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import DeckList from './components/DeckList'
+import DeckDetail from './components/DeckDetail'
+import DeckAdd from './components/DeckAdd'
+
+function DeckStatusBar({ backgroundColor, ...props }){
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
+
+const Tabs = createTabNavigator(
+  {
+    "DECKS": DeckList,
+    "NEW DECK": DeckAdd,
+  },
+  {
+    tabBarOptions: {
+      showIcon: false,
+      activeTintColor: '#000',
+      inactiveTintColor: '#ccc',
+      style: {
+        height: 56,
+        backgroundColor: '#fff',
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1,
+      },
+    },
+  }
+)
+
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: Tabs,
+    navigationOptions: {
+      header: null
+    }
+  },
+  DeckDetail: {
+    screen: DeckDetail,
+    navigationOptions: {
+      headerTintColor: '#fff',
+      headerStyle: {
+        backgroundColor: '#000',
+      }
+    }  
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <DeckStatusBar backgroundColor={'#000'} barStyle='light-content'/>
+        <MainNavigator/>
       </View>
     )
   }
--- components/DeckList.js
+++ components/DeckList.js
@@ -1,11 +1,47 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList, Platform } from 'react-native'
 
 export default class DeckList extends React.Component {
+  deck = {
+    React: {
+      title: 'React',
+      questions: [
+        {
+          question: 'What is React?',
+          answer: 'A library for managing user interfaces'
+        },
+        {
+          question: 'Where do you make Ajax requests in React?',
+          answer: 'The componentDidMount lifecycle event'
+        }
+      ]
+    },
+    JavaScript: {
+      title: 'JavaScript',
+      questions: [
+        {
+          question: 'What is a closure?',
+          answer: 'The combination of a function and the lexical environment within which that function was declared.'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <FlatList
+          data={Object.keys(this.deck).map((id) => this.deck[id])}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item}) => 
+            (
+              <View style={styles.item}>
+                <Text style={styles.deckTitle}>{item.title}</Text>
+                <Text style={styles.deckText}>{item.questions.length} cards</Text>
+              </View>
+            )
+          }
+        />
       </View>
     )
   }
@@ -14,5 +50,39 @@ export default class DeckList extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
+  },
+  item: {
+      backgroundColor: '#fff',
+      borderRadius: Platform.OS === 'ios' ? 16 : 2,
+      padding: 20,
+      height: 160,
+      marginLeft: 15,
+      marginRight: 15,
+      justifyContent: 'center',
+      shadowRadius: 3,
+      shadowOpacity: 0.8,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+          width: 0,
+          heigth: 3,
+      },
+      borderBottomColor: 'black',
+      borderBottomWidth: 1,
+  },
+  noDataText: {
+      fontSize: 20,
+      paddingTop: 20,
+      paddingBottom: 20,
+  },
+  deckTitle: {
+    color: 'black',
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  deckText: {
+    textAlign: 'center',
+    color: '#666',
+    fontSize: 15,
   },
 })
